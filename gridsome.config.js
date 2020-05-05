809e8fc7291346c04c413078d1e39d2e2c178fb3
piotr-s <piotr.slawinski1@gmail.com>
2020-05-05 20:56:53 +0200

    Yet another commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Piotr Slawinski',
+  siteName: 'slawinski.dev',
   plugins: [
     {
       use: '@gridsome/source-filesystem',
