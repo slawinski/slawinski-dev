5b168f43ad8f3fbe697abe328cf0cc7bd8129e4f
piotr-s <piotr.slawinski1@gmail.com>
2020-05-08 22:54:59 +0200

    Add "Projects" page

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,7 +5,8 @@
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/">Blog</g-link>
+        <g-link class="nav__link" to="/projects">Projects</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
