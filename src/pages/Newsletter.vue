775ffadadaa023797a04950037d846578042ab4c
piotr-s <piotr.slawinski1@gmail.com>
2020-05-14 14:29:27 +0200

    Yet another styling update

--- src/pages/Newsletter.vue
+++ src/pages/Newsletter.vue
@@ -1,10 +1,13 @@
 <template>
   <Layout>
     <h1 class="text-headline leading-none font-publicsans font-bold tracking-tight text-4xl sm:text-7xl mb-10">Sign up and I'll email you whenever anything new comes up on the blog</h1>
-    <form class="flex justify-center items-center">
-      <input type="email" v-model="email" class="py-1 px-3 shadow appearance-none border rounded text-xl sm:text-5xl text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your email adress" required>
-      <button @click.prevent="submit" class="sm:mt-0 ml-4 py-1 px-1 bg-transparent hover:bg-button text-button text-xl sm:text-5xl font-publicsans font-bold hover:text-white border border-button hover:border-transparent rounded" >Subscribe</button>
-    </form>  </Layout>
+    <div>
+      <form class="flex justify-center items-center">
+        <input type="email" v-model="email" class="py-1 px-3 w-full shadow appearance-none border rounded text-xl sm:text-5xl text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your email adress" required>
+        <button @click.prevent="submit" class="sm:mt-0 ml-4 py-1 px-1 bg-transparent hover:bg-button text-button text-xl sm:text-5xl font-publicsans font-bold hover:text-white border border-button hover:border-transparent rounded" >Subscribe</button>
+      </form>
+    </div>
+  </Layout>
 </template>
 
 <script>
