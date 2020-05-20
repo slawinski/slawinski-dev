368406215110174584c29aeabd1acada6b5ea520
piotr-s <piotr.slawinski1@gmail.com>
2020-05-20 14:30:31 +0200

    Yet another test if imported styles work in production

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import '@/assets/tailwind.css'
-
+require('~/main.css')
 require('typeface-public-sans')
 require('typeface-pt-sans')
+
+import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
