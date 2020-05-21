8bb249f6f363955f17aa61655a46c35d374193ae
piotr-s <piotr.slawinski1@gmail.com>
2020-05-21 11:23:11 +0200

    Modify the contact form email body

--- functions/sendmail/sendmail.js
+++ functions/sendmail/sendmail.js
@@ -1,15 +1,15 @@
 const client = require("@sendgrid/mail")
 const { SENDGRID_API_KEY, SENDGRID_TO_EMAIL, SENDGRID_FROM_EMAIL } = process.env
 
-exports.handler = async function(event, context, callback) {
-  const { message, senderEmail } = JSON.parse(event.body)
+exports.handler = async function (event, context, callback) {
+  const { message, senderEmail, senderName } = JSON.parse(event.body)
   client.setApiKey(SENDGRID_API_KEY)
 
   const data = {
     to: SENDGRID_TO_EMAIL,
     from: SENDGRID_FROM_EMAIL,
-    subject: `New message from ${senderEmail}`,
-    html: `Hey, you\'ve sent an email from Netlify Functions<br/>Message: ${message}`
+    subject: `New message from ${senderName} (${senderEmail})`,
+    html: message
   }
 
   try {
