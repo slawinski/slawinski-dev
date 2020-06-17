833bef9f9a59002a9e18c61e0f616b4ecc1cd8cd
piotr-s <piotr.slawinski1@gmail.com>
2020-06-17 08:13:18 +0200

    asdf

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -36,7 +36,7 @@ export default {
         cloudName: 'slawinski-dev',
         imagePublicID: 'social-image_dju5tn',
         font: 'helvetica',
-        textLeftOffset: 'null',
+        textLeftOffset: null,
       });
     }
   },
