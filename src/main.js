441e90a661c7e7d5d584939dc0f74ea207ad8d09
piotr-s <piotr.slawinski1@gmail.com>
2020-07-24 07:23:26 +0200

    Add canonical url to fix linkedin social card

--- src/main.js
+++ src/main.js
@@ -16,47 +16,43 @@ export default function (Vue, { router, head, isClient }) {
   // Add attributes to BODY tag
   head.bodyAttrs = { class: 'bg-background leading-normal' };
 
-  head.meta.push({
-    key: 'og:title',
-    name: 'og:title',
-    content: 'slawinski.dev',
-  });
-
-  head.meta.push({
-    key: 'og:type',
-    name: 'og:type',
-    content: 'article',
-  });
-
-  head.meta.push({
-    key: 'og:description',
-    name: 'og:description',
-    content: 'Piotr Slawinski thinks you might like it',
-  });
-
-  head.meta.push({
-    key: 'og:image',
-    name: 'og:image',
-    content: process.env.SOCIAL_IMAGE_LINK,
-  });
-
-  head.meta.push({
-    key: 'twitter:description',
-    name: 'twitter:description',
-    content: 'Piotr Slawinski thinks you might like it',
-  });
-
-  head.meta.push({
-    key: 'twitter:card',
-    name: 'twitter:card',
-    content: 'summary_large_image',
-  });
-
-  head.meta.push({
-    key: 'twitter:image',
-    name: 'twitter:image',
-    content: process.env.SOCIAL_IMAGE_LINK,
-  });
+  head.meta.push(
+    {
+      key: 'og:title',
+      name: 'og:title',
+      content: 'slawinski.dev',
+    },
+    {
+      key: 'og:type',
+      name: 'og:type',
+      content: 'article',
+    },
+    {
+      key: 'og:description',
+      name: 'og:description',
+      content: 'Piotr Slawinski thinks you might like it',
+    },
+    {
+      key: 'og:image',
+      name: 'og:image',
+      content: process.env.SOCIAL_IMAGE_LINK,
+    },
+    {
+      key: 'twitter:description',
+      name: 'twitter:description',
+      content: 'Piotr Slawinski thinks you might like it',
+    },
+    {
+      key: 'twitter:card',
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      key: 'twitter:image',
+      name: 'twitter:image',
+      content: process.env.SOCIAL_IMAGE_LINK,
+    },
+  );
 
   router.beforeEach((to, _from, next) => {
     head.meta.push({
@@ -65,5 +61,10 @@ export default function (Vue, { router, head, isClient }) {
       content: process.env.GRIDSOME_BASE_PATH + to.path,
     });
     next();
+  });
+
+  head.link.push({
+    rel: 'canonical',
+    href: 'https://slawinski.dev',
   });
 }
